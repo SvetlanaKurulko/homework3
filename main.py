11fdb047ae5491055e1b5adf3ecee599b93e81ab
ПК <svetochka116236@gmail.com>
2023-12-20 12:45:00 +0200

    implimented third task

--- main.py
+++ main.py
@@ -49,3 +49,30 @@
 #
 #     except ValueError:
 #         print("Невірне введення. Будь ласка, введіть числа.")
+
+#####
+
+# while True:
+#     try:
+#         num1 = float(input("Введіть перше число: "))
+#         num2 = float(input("Введіть друге число: "))
+#         operation = input("Введіть операцію (+, -, *, /): ")
+#
+#         if operation == '+':
+#             result = num1 + num2
+#         elif operation == '-':
+#             result = num1 - num2
+#         elif operation == '*':
+#             result = num1 * num2
+#         elif operation == '/':
+#             if num2 == 0:
+#                 result = "Помилка: ділення на нуль"
+#             else:
+#                 result = num1 / num2
+#         else:
+#             result = "Невірна операція"
+#
+#         print("Результат:", result)
+#
+#     except ValueError:
+#         print("Невірне введення. Будь ласка, введіть числа.")
